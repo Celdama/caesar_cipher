d88f20175025c7a19f63556ea8835f6eb8167930
Celdama <celdama.dev@gmail.com>
2022-05-11 18:07:02 +0200

    first version

--- cipher.rb
+++ cipher.rb
@@ -1,9 +1,30 @@
-def caesar_cipher(string, _value)
-  # get string
-  # check if method exist to transform string in integer
-  # if not, create an hash with alphabet to get value
-  # return string update
-  string
+# rubocop-disabled
+require 'pry'
+
+class CaesarCipher
+  def translate(string, shift)
+    shifted = []
+    array = string.chars
+
+    array.each do |letter|
+      ascii = letter.ord
+
+      if ascii.between?(65, 90) 
+        ascii + shift > 90 ? shifted << 65 + (ascii + shift - 91) : shifted << ascii + shift
+      elsif  letter.ord.between?(97, 122)
+        ascii + shift > 122 ? shifted << 97 + (ascii + shift - 123) : shifted << ascii + shift
+      else 
+        shifted << ascii
+      end
+    end
+    translated_string = ""
+    shifted.each { | l | translated_string << l.chr}
+    p translated_string
+  end
 end
 
-puts caesar_cipher('What a string!', 5)
+code = CaesarCipher.new
+
+code.translate('AaBbCc - DdEeFf', 10)
+code.translate('What a string!', 5)
+code.translate('', 5)
