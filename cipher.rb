ceba6fc57afdbac44903903635676ee64bb03538
Celdama <celdama.dev@gmail.com>
2022-05-11 14:47:24 +0200

    cipher: added pseudo code

--- cipher.rb
+++ cipher.rb
@@ -1,3 +1,9 @@
-def caesar_cipher string
+def caesar_cipher(string, _value)
+  # get string
+  # check if method exist to transform string in integer
+  # if not, create an hash with alphabet to get value
+  # return string update
+  string
+end
 
-end 
+puts caesar_cipher('What a string!', 5)
